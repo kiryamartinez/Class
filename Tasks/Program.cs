340b0fdeff4da92606b22c965e01b5e5d5e9cdc9
Kirya Martinez <kirilltitov-n@mail.ru>
2021-11-05 20:57:01 +0600

    Задачи

---
+++ Tasks/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Виталий");
+            User user2 = new User("Александр");
+            List list = new List(new Task[] {new Task(user,"Помыть пол"), new Task(user2,"Купить еду") });
+            list.ShowAllTasks();
+        }
+    }
+
+    class User
+    {
+        public string Name;
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
+    class Task
+    {
+        public User Worker;
+        public string Description;
+        public Task(User worker, string desctiprion)
+        {
+            Worker = worker;
+            Description = desctiprion;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Ответсвенный: " + Worker.Name + "\nОписание задачи: " + Description);
+        }
+    }
+    class List
+    {
+        public Task[] Tasks;
+        public List(Task[] tasks)
+        {
+            Tasks = tasks;
+        }
+        public void ShowAllTasks()
+        {
+            for (int i = 0; i < Tasks.Length; i++)
+            {
+                Tasks[i].ShowInfo();
+            }
+        }
+    }
+}
